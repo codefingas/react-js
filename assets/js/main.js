5cedc21dea15f92d4e8586231d3194de8e409502
Ezekiel Saturday <ezekielsaturday@Grooves-MacBook-Pro.local>
2019-11-13 17:15:42 +0100

    explored how events can be used in react

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,29 @@
 class App extends React.Component {
-    state = {
-        name : 'Ryu',
-        age : 35
-    }
-    render() {
+        state = {
+            name : 'Ryu',
+            age : 35
+        }
+
+        handleClick(e) {//this function fires everytime a user clicks
+            console.log(e.target)
+        }
+
+        handleMouseHover(e) {//this function fires everytime  a user hovers over the element
+            console.log(e.target, e.pageX); //logging the target element and the 
+        }
+
+        handleCopy(e) {//this function fires every time a user tries to copy stuff
+            console.log('try being original for once');
+        }
+
+        render() {
         return (
             <div className="app-content">
                 <h1>Hey, ninjas</h1>
                 <p>My name is : {this.state.name} and i am {this.state.age}</p>
+                <button onClick={this.handleClick}>Click me</button>
+                <button onMouseOver={this.handleMouseHover}>Hover me</button>
+                <p onCopy={this.handleCopy}>What we think, we become</p>
             </div>
         )
     }
