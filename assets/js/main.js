9a4246b9a65ddf28dea5e773801786fe50b6fba2
Ezekiel Saturday <ezekielsaturday@Grooves-MacBook-Pro.local>
2019-11-13 15:04:53 +0100

    took a try at states

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,13 @@
 class App extends React.Component {
+    state = {
+        name : 'Ryu',
+        age : 35
+    }
     render() {
         return (
             <div className="app-content">
                 <h1>Hey, ninjas</h1>
-                <p>{Math.random() * 10}</p>
+                <p>My name is : {this.state.name} and i am {this.state.age}</p>
             </div>
         )
     }
