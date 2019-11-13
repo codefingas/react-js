b059e18c4fba3183254b2b15400452823e4936a8
Ezekiel Saturday <ezekielsaturday@Grooves-MacBook-Pro.local>
2019-11-13 18:14:15 +0100

    looked into forms on reactjs

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,30 +4,25 @@ class App extends React.Component {
             age : 35
         }
 
-        handleClick = (e) => {//this function fires everytime a user clicks
-            // console.log(e.target)
+        handleChange = (e) => {
             this.setState({
-                name : 'Yoshi'
+                name : e.target.value
             })
-            console.log(this.state);
         }
 
-        handleMouseHover = (e) => {//this function fires everytime  a user hovers over the element
-            console.log(e.target, e.pageX); //logging the target element and the 
-        }
-
-        handleCopy = (e) => {//this function fires every time a user tries to copy stuff
-            console.log('try being original for once');
+        handleSubmit = (e) => {
+            e.preventDefault();
+            console.log('form submitted', this.state.name)
         }
 
         render() {
         return (
             <div className="app-content">
-                <h1>Hey, ninjas</h1>
-                <p>My name is : {this.state.name} and i am {this.state.age}</p>
-                <button onClick={this.handleClick}>Click me</button>
-                <button onMouseOver={this.handleMouseHover}>Hover me</button>
-                <p onCopy={this.handleCopy}>What we think, we become</p>
+                <h1> My name is : {this.state.name} </h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" onChange={this.handleChange}/>
+                    <button >Submit</button>
+                </form>
             </div>
         )
     }
